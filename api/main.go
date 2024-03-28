285ed1833fc48f80842fc173e655ad1d637702d2
Nuttavut Thongjor <nthongjor@LAPTOP-HEF902DE>
2024-03-28 14:01:47 +0700

    add Datadog

--- api/main.go
+++ api/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/babelcoder-dummy/intro-devops/api/config"
 	"github.com/babelcoder-dummy/intro-devops/api/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	fibertrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gofiber/fiber.v2"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 func main() {
 	app := fiber.New()
+
+	if os.Getenv("APP_ENV") == "production" {
+		tracer.Start()
+		defer tracer.Stop()
+		
+		app.Use(fibertrace.Middleware(fibertrace.WithServiceName("api")))
+	}
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
